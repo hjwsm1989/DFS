3961c209d002a91924a424124a746f1822402a57
hjwsm1989 <hjwsm1989@gmail.com>
2011-09-28 21:10:26 +0800

    begin to do the mutex file

--- Mutex.h
+++ Mutex.h
@@ -1,5 +1,17 @@
-#include <unstd.h>
+/* I want to beacome a hacker!
+ * At first, i should try to write as much as possible codes.
+ * Many times, i think i should insist on this job!
+ * email:hjwsm1989@gmail.com
+ */
+
+#include <unistd.h>
+#include <pthread.h>
+
 int main()
 {
+	pthread_mutex_t &lock;
+	Lock();
+	
+	Unlock();
 	return 0;
 }
