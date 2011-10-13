1c56560d09316f1326adc1353dd976b589bf61fa
hjwsm1989 <hjwsm1989@gmail.com>
2011-10-13 21:15:52 +0800

    modify it to used to the max common fractor

--- maxcommonfactor.c
+++ maxcommonfactor.c
@@ -1,32 +1,41 @@
 /* write the program to test examples in <<The Art of Computer Programming>>
- * huangjun 2011-10-13
- * hjwsm1989@gmail.com
- */
+ *  * huangjun 2011-10-13
+ *   * hjwsm1989@gmail.com
+ *    */
 #include <stdio.h>
 #include <stdlib.h>
 
+int maxcommonfactor(int m, int n)
+{
+        int r = 0;
+
+        if(m < n){
+                int temp;
+                temp = m;
+                m = n;
+                n = temp;
+        }
+                                                                           
+        while (1) {
+                r = m % n;
+                if(r == 0)
+                        break;
+                m = n;
+                n = r;
+
+        };
+                                                                                                                                    
+        return n;
+}
+
 int main ()
 {
-  int m, n, r;
-  r = 0;
-  printf("Please Enter m and n \n");
-  scanf("%d%d", m, n);
+        int m, n, r;
+        printf("m = ");
+        scanf("%d", &m);
+        printf("n = ");
+        scanf("%d", &n);
+        r = maxcommonfactor(m,n);
+        printf("max common factor = %d \n", r);
 
-  if (m < n)
-  {
-    int temp;
-    temp = m;
-    m = n;
-    n = temp;
-  }
-  
-  while (1) {
-    r = m % n;
-    if (r == 0)
-      return n;
-    m = n;
-    n = r;
-  };
-  
-  return 0;
 }
