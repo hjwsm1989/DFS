050dccb7a3e40deae69993491f60242243b3851a
hjwsm1989 <hjwsm1989@gmail.com>
2011-10-13 13:30:02 +0800

    first chapter, and first example of max common factor

---
+++ maxcommonfactor.c
@@ -0,0 +1,24 @@
+/* write the program to test examples in <<The Art of Computer Programming>>
+ * huangjun 2011-10-13
+ * hjwsm1989@gmail.com
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+  int m, n, r;
+  r = 0;
+  printf("Please Enter m and n \n");
+  scanf("%d%d", m, n);
+  
+  while (1) {
+    r = m % n;
+    if (r == 0)
+      return n;
+    m = n;
+    n = r;
+  };
+  
+  return 0;
+}
